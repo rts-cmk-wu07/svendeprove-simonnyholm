8609a7945ef2e8fdb73bad9177da500d439a9665
Simon Nyholm Sørensen <simonnyholm@htomail>
2023-03-14 15:55:28 +0100

    add colors + fonts to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryTextColor: "#EAEAEA",
+        primaryPurple: "#5E2E53",
+        secondaryPurple: "#E1A1E9",
+        itemTextColor: "#000000",
+      },
+      fontFamily: {
+        primaryFont: ["Ubuntu", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        racing: ["Racing Sans One", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
