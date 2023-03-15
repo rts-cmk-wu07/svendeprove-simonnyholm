919a709249e72d70628594cb9c21f30335c8689e
Simon Nyholm Sørensen <simonnyholm@htomail>
2023-03-15 13:57:19 +0100

    add navlink

--- src/pages/ActivityDetails.jsx
+++ src/pages/ActivityDetails.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import DependentReq from "../components/DependentReq.jsx";
 import axios from "axios";
+import { NavLink } from "react-router-dom";
 
 const ActivityDetails = () => {
   const { id } = useParams();
@@ -45,12 +46,36 @@ const ActivityDetails = () => {
       {activityDetail && (
         <>
           <section>
-            <article>
-              <div>
-                <img src={activityDetail.asset.url} alt="" />
-              </div>
-              <h1>{activityDetail.name}</h1>
-              <p></p>
+            <div className="h-[55vh]">
+              <img
+                className="relative object-cover h-full w-full"
+                src={activityDetail.asset.url}
+                alt=""
+              />
+            </div>
+
+            <NavLink
+              to="/login"
+              className="flex justify-center absolute top-[45vh] right-[10vw] bg-primaryPurple text-primaryTextColor text-[18px] pr-4 pl-4 pt-3 pb-3 w-[249px] h-[54px] rounded-[10px] drop-shadow-[0_6px_5px_rgba(0,0,0,0.25)]"
+            >
+              <p>Log på for tilmelding</p>
+            </NavLink>
+
+            <article className="ml-[10vw] mr-[6vw] pt-[8vw] h-[45vh]">
+              <h1 className="text-[24px] text-primaryTextColor">
+                {activityDetail.name}
+              </h1>
+              <p className="text-[18px] text-primaryTextColor pt-1">
+                {activityDetail.minAge}-{activityDetail.maxAge} år
+              </p>
+
+              <p className="text-[18px] text-primaryTextColor pt-1">
+                <span className="capitalize">{activityDetail.weekday} </span>
+                kl. {activityDetail.time}
+              </p>
+              <p className="text-[18px] text-primaryTextColor pt-5">
+                {activityDetail.description}
+              </p>
             </article>
           </section>
         </>
@@ -69,3 +94,9 @@ const ActivityDetails = () => {
 };
 
 export default ActivityDetails;
+
+/*
+            <button className="absolute top-[45vh] right-[10vw] bg-primaryPurple text-primaryTextColor text-[18px] pr-4 pl-4 pt-3 pb-3 w-[249px] h-[54px] rounded-[10px] drop-shadow-[0_6px_5px_rgba(0,0,0,0.25)]">
+              Log på for tilmelding
+            </button>
+*/
