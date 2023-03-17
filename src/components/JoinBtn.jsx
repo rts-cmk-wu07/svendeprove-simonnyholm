0e1e685c35deee827b210d9f147beaea86817cd3
Simon Nyholm Sørensen <simonnyholm@htomail>
2023-03-17 11:14:58 +0100

    complete user validation

--- src/components/JoinBtn.jsx
+++ src/components/JoinBtn.jsx
@@ -1,25 +1,22 @@
 import { useState, useContext, useEffect } from "react";
-
 import axios from "axios";
-import { NavLink } from "react-router-dom";
 import { TokenContext } from "../contexts/TokenProvider";
 import { UserDataContext } from "../contexts/UserDataProvider";
+import QuitBtn from "./QuitBtn";
+import GetUserData from "./GetUserData";
+import { NavLink } from "react-router-dom";
 
 const JoinBtn = ({ id, detail }) => {
   const { token } = useContext(TokenContext);
   const { userData } = useContext(UserDataContext);
   const [hasJoined, setHasJoined] = useState(false);
-  const [quitModal, setQuitModal] = useState(false);
-
+  const [renderRequest, setRenderRequest] = useState(false);
+  const [dayOccupied, setDayOccupied] = useState(false);
+  const [tooOld, setTooOld] = useState(false);
+  const [activityFull, setActivityFull] = useState(false);
+  const [tooYoung, setTooYoung] = useState(false);
+  const [userNotValid, setUserNotValid] = useState(false);
   console.log("userActivities", userData.activities);
-  console.log("details in join-weekday", detail.weekday);
-
-  function filterByUserId(item) {
-    if (item.id === userData.id) {
-      return true;
-    }
-  }
-  const hasJoinedActivity = detail.users.filter(filterByUserId);
 
   function filterByActivityWeekday(item) {
     if (item.weekday === detail.weekday) {
@@ -34,15 +31,39 @@ const JoinBtn = ({ id, detail }) => {
 
   function validateUser(event) {
     if (hasActivitySameWeekday.length >= 1) {
-      console.log("Du har allerede et hold samme dag");
+      setDayOccupied(true);
+      setUserNotValid(true);
       //fejlbesked
       return;
     } else {
-      joinHandler();
+      if (userData.age >= detail.minAge) {
+        if (userData.age > detail.maxAge) {
+          console.log("Du er for gammel til at deltage");
+          setTooOld(true);
+          setUserNotValid(true);
+          return;
+        } else {
+          if (detail.users.length >= detail.maxParticipants) {
+            setActivityFull(true);
+            setUserNotValid(true);
+            console.log("Holdet er desværre fyldt op");
+            //fejlbesked
+            return;
+          } else {
+            joinHandler();
+          }
+        }
+      } else {
+        setTooYoung(true);
+        console.log("Du er ikke gammel nok til at deltage");
+        setUserNotValid(true);
+        //fejlbesked
+        return;
+      }
     }
   }
 
-  async function joinHandler(event) {
+  async function joinHandler() {
     try {
       const response = await axios.post(
         `http://localhost:4000/api/v1/users/${token.userId}/activities/${id}`,
@@ -61,50 +82,25 @@ const JoinBtn = ({ id, detail }) => {
     } catch (error) {
       console.log(error);
     } finally {
+      setRenderRequest(true);
       console.log("joined");
+      setHasJoined(true);
     }
   }
 
-  async function quitHandler(event) {
-    try {
-      const response = await axios.delete(
-        `http://localhost:4000/api/v1/users/${token.userId}/activities/${id}`,
-        {
-          headers: {
-            authorization: "Bearer " + token.token,
-          },
-        }
-      );
-
-      if (response.status === 200) {
-        setHasJoined(false);
-      }
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      console.log("user has quit this activity");
-      setQuitModal(false);
-    }
-  }
-
-  /*
-  if (hasJoinedActivity.length >= 1) {
-    console.log("Du er allerede på holdet til at begynde med");
-    setHasJoined(true);
-    //fejlbesked
-  }
-  */
+  // {hasJoined ? (row.isGrouped ? "GroupedRow" : "") : "SelectedRow"}
 
   return (
     <>
       {hasJoined ? (
-        <button
-          onClick={(e) => setQuitModal(true)}
+        <QuitBtn id={id} detail={detail} />
+      ) : userNotValid ? (
+        <NavLink
+          to="/aktiviteter"
           className="flex justify-center absolute top-[45vh] right-[10vw] bg-primaryPurple text-primaryTextColor text-[18px] pr-4 pl-4 pt-3 pb-3 w-[249px] h-[54px] rounded-[10px] drop-shadow-[0_6px_5px_rgba(0,0,0,0.25)]"
         >
-          Forlad
-        </button>
+          Se vores andre aktiviteter
+        </NavLink>
       ) : (
         <button
           onClick={validateUser}
@@ -113,15 +109,24 @@ const JoinBtn = ({ id, detail }) => {
           Tilmeld dig
         </button>
       )}
-      {quitModal && (
-        <div className="absolute w-10 h-5 bottom-[300px] left-8 bg-secondaryPurple">
-          <p>Ønsker du at forlade holdet?</p>
-          <div>
-            <button onClick={quitHandler}>Ja</button>
-            <button onClick={(e) => setQuitModal(false)}>Nej</button>
-          </div>
+      {dayOccupied && (
+        <div className="flex justify-center absolute top-[40vh] right-[10vw] bg-secondaryPurple text-itemTextColor text-[18px] pr-4 pl-4 pt-3 pb-3 w-[249px] h-[54px] rounded-[10px] drop-shadow-[0_6px_5px_rgba(0,0,0,0.25)]">
+          Du har allerede en aktivitet om {detail.weekday}en.
         </div>
       )}
+      {tooOld && (
+        <div className="flex justify-center absolute top-[20vh] right-[10vw] bg-secondaryPurple text-itemTextColor text-[18px] pr-4 pl-4 pt-3 pb-3 w-[249px] h-[190px] rounded-[10px] drop-shadow-[0_6px_5px_rgba(0,0,0,0.25)]">
+          Du er desværre for gammel til at deltage i {detail.name}. Heldigvis
+          har vi mange andre aktiviteter til dig.
+        </div>
+      )}
+      {activityFull && (
+        <div className="flex justify-center absolute top-[20vh] right-[10vw] bg-secondaryPurple text-itemTextColor text-[18px] pr-4 pl-4 pt-3 pb-3 w-[249px] h-[190px] rounded-[10px] drop-shadow-[0_6px_5px_rgba(0,0,0,0.25)]">
+          {detail.name} er desværre fyldt helt op. Heldigvis har vi mange andre
+          aktiviteter til dig.
+        </div>
+      )}
+      {renderRequest && <GetUserData />}
     </>
   );
 };
